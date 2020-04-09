01f091d6ec4962633430ab0353db046aef2a60e1
Robert <46203504+rdweston1995@users.noreply.github.com>
2020-04-08 23:18:44 -0700

    Added percent grade

--- axiosTest.js
+++ axiosTest.js
@@ -2,8 +2,8 @@ const axios = require("axios");
 
 const axiosObj = {
     key: '200723231-608d19aa9a3f132e824907a2db30d730',
-    lat: '47.4165',
-    lon: '-122.1741'
+    lat: '47.4249',
+    lon: '-121.433'
 }
 
 let url = `https://www.hikingproject.com/data/get-trails?lat=${axiosObj.lat}&lon=${axiosObj.lon}&maxDistance=10&key=${axiosObj.key}`;
@@ -24,10 +24,12 @@ axios.get(url).then((res) => {
      */
     
     for(let i = 0; i < res.data.trails.length; i++) {
+        let percentGrade = (res.data.trails[i].high / res.data.trails[i].low).toFixed(2);
         console.log(res.data.trails[i].name + " | " + res.data.trails[i].stars);
         console.log(res.data.trails[i].summary);
         console.log(res.data.trails[i].location);
-        console.log(res.data.trails[i].length + " | " + res.data.trails[i].ascent + " | " + res.data.trails[i].high + " | " + res.data.trails[i].low);
+        console.log(res.data.trails[i].length + " | " + res.data.trails[i].ascent + " | " + res.data.trails[i].low + " | " + res.data.trails[i].high);
+        console.log("Percent Grade: " + percentGrade);
         console.log("=============================================================================");
     }
 }).catch((err) => {
