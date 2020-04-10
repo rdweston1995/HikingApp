f4d6856ae8e667c21afe7c3f39f8834a93966c53
Robert <robert.d.weston@gmail.com>
2020-04-09 18:45:14 -0700

    Added google geocode api for location converting

---
+++ geocodeTest.js
@@ -0,0 +1,45 @@
+// const geocode = require("react-geocode");
+const axios = require("axios");
+
+// geocode.setApiKey("AIzaSyADTa33SpEuErTRKXJ8UWucpoEUOnYV6wg");
+
+// geocode.fromAddress("Space Needle").then(
+//     res => {
+//         const { lat, long } = res.results[0].geometry.location;
+//         console.log(lat, long);
+//     }, err => {
+//         console.log(err);
+//     }
+// )
+
+// let location = "Seattle";
+
+
+// let url = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyADTa33SpEuErTRKXJ8UWucpoEUOnYV6wg"
+// let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=AIzaSyADTa33SpEuErTRKXJ8UWucpoEUOnYV6wg`;
+
+// axios.get(url).then((res) =>{ 
+//     console.log(res.data.results[0]);
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+// async function getLatLon(location){
+//     axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=AIzaSyADTa33SpEuErTRKXJ8UWucpoEUOnYV6wg`).then((res) => {
+//         return res.data.results[0].
+//     });
+// }
+
+class Geocode {
+    async getLatlon(location) {
+        return new Promise((reject, resolve) =>{
+            axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=AIzaSyADTa33SpEuErTRKXJ8UWucpoEUOnYV6wg`).then((res) => {
+                resolve(res.data.results[0]);
+            }).catch((err) => {
+                reject(err);
+            })
+        });
+    }
+}
+
+module.exports = Geocode;
