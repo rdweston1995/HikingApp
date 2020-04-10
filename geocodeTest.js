dccd7b9bc38d78d401b3095108a831740d601e52
Robert <46203504+rdweston1995@users.noreply.github.com>
2020-04-09 21:59:25 -0700

    Cleaned up test file :

--- geocodeTest.js
+++ geocodeTest.js
@@ -2,10 +2,6 @@ const axios = require("axios");
 
 class Geocode {
     async getLatLng(location) {
-        // let url = 
-        // return `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.GoogleAPIKey}`;
-        console.log(process.env.HOME);
-        console.log(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.GOOGLE_API_KEY}`);
         return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.GOOGLE_API_KEY}`).then((res) => {
             return res.data.results;
         }).catch((err) => {
--- test.js
+++ test.js
@@ -3,18 +3,10 @@ const geocode = require("./geocodeTest.js");
 const axiosTest = require("./axiosTest.js");
 
 new geocode().getLatLng("Mount Rainier National Park").then((data) => {
-    // console.log(data[0].formatted_address);
-    // console.log(data[0].formatted_address + "\t" + data[0].geometry.location.lat.toFixed(4) + ", " + data[0].geometry.location.lng.toFixed(4));
-    // console.log(data[0].geometry.location);
-    // console.log(data[0].geometry.location.lat.toFixed(4));
-    // console.log(data[0].geometry.location.lng.toFixed(4));
-    console.log(data);
-    // new axiosTest().hikingData(data[0].geometry.location.lat.toFixed(4), data[0].geometry.location.lng.toFixed(4)).then((results) => {
-        // console.log(results);
-        // for(let i = 0; i < results.length; i++){
-            // console.log(results[i].name);
-        // }
-    // });
-})
-
-// new geoCode().getLatLng("MTNR");
+    console.log(data[0].formatted_address + "\t" + data[0].geometry.location.lat.toFixed(4) + ", " + data[0].geometry.location.lng.toFixed(4));
+    new axiosTest().hikingData(data[0].geometry.location.lat.toFixed(4), data[0].geometry.location.lng.toFixed(4)).then((results) => {
+        for(let i = 0; i < results.length; i++){
+            console.log(results[i].name);
+        }
+    });
+});
