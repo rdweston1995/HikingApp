1d9b12bb8b2c75b2a0052cf06a98fda8a4f0dc30
Robert <46203504+rdweston1995@users.noreply.github.com>
2020-04-09 21:57:22 -0700

    Added env file

--- axiosTest.js
+++ axiosTest.js
@@ -1,46 +1,9 @@
 //  https://www.hikingproject.com/data
-
 const axios = require("axios");
-
-// const axiosObj = {
-//     key: '200723231-608d19aa9a3f132e824907a2db30d730',
-//     lat: '47.4249',
-//     lon: '-121.433'
-// }
-
-// let url = `https://www.hikingproject.com/data/get-trails?lat=${axiosObj.lat}&lon=${axiosObj.lon}&maxDistance=10&key=${axiosObj.key}`;
-
-// axios.get(url).then((res) => {
-// console.log(res.data.trails.length);
-// console.log(res.data.trails[0]);
-/**
- * Name
- * Summary
- * stars
- * location
- * length
- * ascent
- * high
- * low
- * 
- */
-
-//     for(let i = 0; i < res.data.trails.length; i++) {
-//         let percentGrade = (res.data.trails[i].high / res.data.trails[i].low).toFixed(2);
-//         console.log(res.data.trails[i].name + " | " + res.data.trails[i].stars);
-//         console.log(res.data.trails[i].summary);
-//         console.log(res.data.trails[i].location);
-//         console.log(res.data.trails[i].length + " | " + res.data.trails[i].ascent + " | " + res.data.trails[i].low + " | " + res.data.trails[i].high);
-//         console.log("Percent Grade: " + percentGrade);
-//         console.log("=============================================================================");
-//     }
-// }).catch((err) => {
-//     console.log(err);
-// })
 
 class axiosTest {
     async hikingData(lat, lng) {
-        return axios.get(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lng}&maxDistance=10&key=200723231-608d19aa9a3f132e824907a2db30d730`).then((res) => {
+        return axios.get(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lng}&maxDistance=10&key=${process.env.HIKING_PROJECT_KEY}`).then((res) => {
             // console.log(res);
             return res.data.trails;
         }).catch((err) => {
--- test.js
+++ test.js
@@ -1,17 +1,20 @@
+require('dotenv').config();
 const geocode = require("./geocodeTest.js");
 const axiosTest = require("./axiosTest.js");
 
-new geocode().getLatLon("Mount Rainier National Park").then((data) => {
+new geocode().getLatLng("Mount Rainier National Park").then((data) => {
     // console.log(data[0].formatted_address);
-    console.log(data[0].formatted_address);
-    console.log(data[0].geometry.location);
+    // console.log(data[0].formatted_address + "\t" + data[0].geometry.location.lat.toFixed(4) + ", " + data[0].geometry.location.lng.toFixed(4));
+    // console.log(data[0].geometry.location);
     // console.log(data[0].geometry.location.lat.toFixed(4));
     // console.log(data[0].geometry.location.lng.toFixed(4));
-
-    new axiosTest().hikingData(data[0].geometry.location.lat.toFixed(4), data[0].geometry.location.lng.toFixed(4)).then((results) => {
+    console.log(data);
+    // new axiosTest().hikingData(data[0].geometry.location.lat.toFixed(4), data[0].geometry.location.lng.toFixed(4)).then((results) => {
         // console.log(results);
-        for(let i = 0; i < results.length; i++){
-            console.log(results[i].name);
-        }
-    });
+        // for(let i = 0; i < results.length; i++){
+            // console.log(results[i].name);
+        // }
+    // });
 })
+
+// new geoCode().getLatLng("MTNR");
