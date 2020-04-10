93eafd85849a46126828d82f15c161c39f5a7d9c
Robert <robert.d.weston@gmail.com>
2020-04-09 19:20:02 -0700

    Working on hike search functionality

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const geocode = require("./geocodeTest.js");
 const axiosTest = require("./axiosTest.js");
 
-new geocode().getLatLon("Seattle").then((data) => {
+new geocode().getLatLon("Mount Rainier National Park").then((data) => {
     // console.log(data[0].formatted_address);
     console.log(data[0].formatted_address);
     console.log(data[0].geometry.location);
@@ -9,6 +9,9 @@ new geocode().getLatLon("Seattle").then((data) => {
     // console.log(data[0].geometry.location.lng.toFixed(4));
 
     new axiosTest().hikingData(data[0].geometry.location.lat.toFixed(4), data[0].geometry.location.lng.toFixed(4)).then((results) => {
-        console.log(results);
+        // console.log(results);
+        for(let i = 0; i < results.length; i++){
+            console.log(results[i].name);
+        }
     });
 })
