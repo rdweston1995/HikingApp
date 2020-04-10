54ebe082fdee69a171c49ba01a6e30e20af49ee1
Robert <46203504+rdweston1995@users.noreply.github.com>
2020-04-09 23:59:38 -0700

    Added outline for home page

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home/home";
 
 class App extends Component {
     render() {
-        return();
+        return(
+            <Home />
+        );
     }
 }
 
