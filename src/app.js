03a48eeec13c947af92feffd5a3a16668182ea6a
Robert <46203504+rdweston1995@users.noreply.github.com>
2020-04-09 23:54:33 -0700

    Added index file in public and set up App.js

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+class App extends Component {
+    render() {
+        return();
+    }
+}
+
+export default App;
