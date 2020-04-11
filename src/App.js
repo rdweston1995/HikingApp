88ba2c76f38dd725816f2cd6a2a655100a567665
Robert <robert.d.weston@gmail.com>
2020-04-10 20:05:54 -0700

    Working on react/bootstrap components for the home page

--- src/App.js
+++ src/App.js
@@ -4,15 +4,6 @@ import './App.css';
 
 import Home from "./pages/home/home";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       {/* <p>Hello World</p> */}
-//       <home />
-//     </div>
-//   );
-// }
-
 class App extends React.Component {
     render(){
         return (
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,39 @@
+//NPM imports
 import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
+import FormControl from "react-bootstrap/FormControl";
 
-function home() {
-    return(
-        <div className="homeRow">
-            <div>Hello World</div>
-        </div>
-    );
+class home extends React.Component {
+    state = {
+        date: new Date()
+    }
+
+
+    render() {
+        return (
+            <Row className="homeRow">
+                <Col>
+                    <h1>Hiking App</h1>
+                    <div>
+                        <InputGroup className="mb-5">
+                            <FormControl aria-describedby="basic-addon2" />
+                            <InputGroup.Append>
+                                <Button variant="outline-secondary">Search</Button>
+                            </InputGroup.Append>
+                        </InputGroup>
+                    </div>
+                </Col>
+                <Col>
+                    <div>
+                        <h2>It is {this.state.date.toString()}</h2>
+                    </div>
+                </Col>
+            </Row>
+        )
+    }
 }
 
 export default home;
