6da2a2f81ffb068f2ff8440234fd07eb169a7175
Robert <46203504+rdweston1995@users.noreply.github.com>
2020-04-10 23:53:37 -0700

    Working on expanding hike search

--- axiosTest.js
+++ axiosTest.js
@@ -2,9 +2,15 @@
 const axios = require("axios");
 
 class axiosTest {
-    async hikingData(lat, lng) {
-        return axios.get(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lng}&maxDistance=10&key=${process.env.HIKING_PROJECT_KEY}`).then((res) => {
-            // console.log(res);
+    async hikingData(lat, lng, maxDist, maxRes, sort, minLen, minStar) {
+        let maxDistance = maxDist || 30;
+        let maxResults = maxRes || 10;
+        let minLength = minLen || 0;
+        let minStars = minStar || 0;
+        // console.log(maxDistance + "\t" + maxResults + "\t" + minLength + "\t" + minStars);
+
+        return axios.get(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lng}&maxDistance=${maxDistance}&maxResults=${maxResults}&minLength=${minLength}&minStars=${minStars}&key=${process.env.HIKING_PROJECT_KEY}`).then((res) => {
+            console.log(res);
             return res.data.trails;
         }).catch((err) => {
             console.log(err);
--- test.js
+++ test.js
@@ -3,10 +3,14 @@ const geocode = require("./geocodeTest.js");
 const axiosTest = require("./axiosTest.js");
 
 new geocode().getLatLng("Mount Rainier National Park").then((data) => {
-    console.log(data[0].formatted_address + "\t" + data[0].geometry.location.lat.toFixed(4) + ", " + data[0].geometry.location.lng.toFixed(4));
+    // console.log(data[0].formatted_address + "\t" + data[0].geometry.location.lat.toFixed(4) + ", " + data[0].geometry.location.lng.toFixed(4));
+    console.log(data);
     new axiosTest().hikingData(data[0].geometry.location.lat.toFixed(4), data[0].geometry.location.lng.toFixed(4)).then((results) => {
         for(let i = 0; i < results.length; i++){
             console.log(results[i].name);
         }
     });
 });
+
+// new axiosTest().hikingData();
+
