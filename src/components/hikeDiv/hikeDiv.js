5551d94d5b97ab1ee41f79a8c7c74d5014779fa9
Robert <46203504+rdweston1995@users.noreply.github.com>
2020-04-15 21:19:09 -0700

    Adding classNames to hikeDiv

--- src/components/hikeDiv/hikeDiv.js
+++ src/components/hikeDiv/hikeDiv.js
@@ -6,13 +6,12 @@ import "./hikeDiv.css";
 function hikes(props) {
     return(
         <div className={props.className} onClick={props.onClick}>
-            <h2>{props.name}</h2>
-            <h3>{props.location}</h3>
-            {/* <p>{props.summary}</p> */}
-            <img src={props.imgSmall} alt={props.name}/>
-            <p>{props.summary}</p>
-            <p>Heighest Point: {props.high}ft<br/>Lowest Point: {props.low}ft<br/>Ascent: {props.ascent}ft<br/>Length: {props.length} miles</p>
-            <p>Rating: {props.stars} / 5</p>
+            <h2 className="hikeName">{props.name}</h2>
+            <h3 className="hikeLocation">{props.location}</h3>
+            <img className="hikeImage" src={props.imgSmall} alt={props.name}/>
+            <p className="hikeSummary">{props.summary}</p>
+            <p className="hikeAlt">Heighest Point: {props.high}ft<br/>Lowest Point: {props.low}ft<br/>Ascent: {props.ascent}ft<br/>Length: {props.length} miles</p>
+            <p className="hikeRating">Rating: {props.stars} / 5</p>
 
         </div>
     )
