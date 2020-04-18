d860d8072b3c18bee7c103432b7adf916df37625
Robert <46203504+rdweston1995@users.noreply.github.com>
2020-04-17 22:40:41 -0700

    Importing css

--- src/components/loginModal/loginModal.js
+++ src/components/loginModal/loginModal.js
@@ -1,3 +1,15 @@
 //Node Imports
 import React from "react";
 import Modal from "react-bootstrap/Modal";
+
+//CSS
+import "./loginModal.css";
+
+function loginModal() {
+    return (
+        <>
+        </>
+    )
+}
+
+export default loginModal;
