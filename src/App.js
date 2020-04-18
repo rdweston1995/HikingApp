1061c793ce362bf172f5afbf588a2650ec7d2e4e
Robert <46203504+rdweston1995@users.noreply.github.com>
2020-04-17 21:48:11 -0700

    Working on google maps

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 //npm
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 //CSS
 import './App.css';
 
 //Pages
 import Home from "./pages/home/home";
+import Map from "./pages/map/map";
 
 class App extends React.Component {
     render(){
         return (
-            <div className="App">
-                <Home />
-            </div>
+            <Router>
+                <Switch>
+                    <Route exact path="/home" component={Home} />
+                    <Route exact path="/map" component={Map} />
+                    <Route component={Home} />
+                </Switch>
+            </Router>
+            // <div className="App">
+                // <Home />
+            // </div>
         )
     }
 }
---
+++ src/pages/map/map.js
@@ -0,0 +1,20 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+
+class Map extends React.Component {
+    render() {
+        return (
+            <div style={{height: '100vh', width: '100%'}}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{key: process.env.REACT_GOOGLE_API_KEY}}
+                    defaultCenter={{lat: 47.4163, lng: -122.174}}
+                    defaultZoom={11}
+                    >
+
+                </GoogleMapReact>
+            </div>
+        )
+    }
+}
+
+export default Map;
