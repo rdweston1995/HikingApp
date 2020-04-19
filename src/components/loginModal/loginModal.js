ab9b35c6e8a6c77f506976653afd7cb2b1124bbd
Robert <46203504+rdweston1995@users.noreply.github.com>
2020-04-18 23:06:49 -0700

    Setting up initial modal

--- src/components/loginModal/loginModal.js
+++ src/components/loginModal/loginModal.js
@@ -1,13 +1,27 @@
 //Node Imports
 import React from "react";
 import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 //CSS
 import "./loginModal.css";
 
 function loginModal() {
+    // const [show, setShow] = useState(false);
+    // const handleClose = () => setShow(false);
+    // const handleShow = () => setShow(true);
     return (
         <>
+            <Modal >
+                <Modal.Header closeButton>
+                    Modal Heading
+                </Modal.Header>
+                <Modal.Body>Modal Body</Modal.Body>
+                <Modal.Footer>
+                    <Button varient="secondary" >Close</Button>
+                    <Button varient="primary" >Submit</Button>
+                </Modal.Footer>
+            </Modal>
         </>
     )
 }
