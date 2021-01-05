016b228e43093f79bd45e1a99819f70aed5d50ba
Robert <robert.d.weston@gmail.com>
2021-01-04 19:02:14 -0800

    Setting up classNames

--- src/components/hikeInfo/hikeInfo.js
+++ src/components/hikeInfo/hikeInfo.js
@@ -5,17 +5,17 @@ import "./hikeInfo.css";
 function hikeInfo(props) {
     return(
         <div className="hikeInfo">
-            <p>{props.hike.name}</p>
-            <p>{props.hike.location}</p>
-            <p>{props.hike.difficulty}</p>
-            <p>{props.hike.low} - {props.hike.high}</p>
-            <p>{props.hike.length}</p>
-            <p>{props.hike.stars}</p>
-            <p>{props.hike.summary}</p>
-            <p>{props.hike.url}</p>
-            <p>{props.hike.conditionDate}</p>
-            <p>{props.hike.conditionDetails}</p>
-            <p>{props.hike.conditionStatus}</p>
+            <h2 className="hikeInfoName">{props.hike.name}</h2>
+            <h3 className="hikeInfoLoc">{props.hike.location}</h3>
+            <p className="hikeInfoDiff">{props.hike.difficulty}</p>
+            <p className="hikeInfoAscent">Low: {props.hike.low}ft - High: {props.hike.high}ft</p>
+            <p className="hikeInfoLeng">{props.hike.length} miles</p>
+            <p className="hikeInfoStars">{props.hike.stars} / 5 stars</p>
+            <p className="hikeInfoSum">{props.hike.summary}</p>
+            <p className="hikeInfoUrl">{props.hike.url}</p>
+            <p className="hikeInfoDate">{props.hike.conditionDate}</p>
+            <p className="hikeInfoDetails">{props.hike.conditionDetails}</p>
+            <p className="hikeInfoStatus">{props.hike.conditionStatus}</p>
         </div>
     );
 };
