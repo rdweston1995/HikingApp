aee0932a517d7c9ce1e7234f8a729e2789ffa639
Robert <robert.d.weston@gmail.com>
2021-02-01 19:29:02 -0800

    Updates

---
+++ precache-manifest.404bcece2e5d3e661e1a9d8386d6ba19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a8bbdaa611af60a4671300aea517ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "223712e4f2d61fc97f99",
+    "url": "/static/css/main.a86a1f96.chunk.css"
+  },
+  {
+    "revision": "31dabfae6d406e031023",
+    "url": "/static/js/2.2bfc7425.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2bfc7425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "223712e4f2d61fc97f99",
+    "url": "/static/js/main.811bd456.chunk.js"
+  },
+  {
+    "revision": "1c6d28d2af0c34bf1ee5",
+    "url": "/static/js/runtime-main.063fee58.js"
+  },
+  {
+    "revision": "fdb48285012cae099f215dbbe4efab3c",
+    "url": "/static/media/homeBackground.fdb48285.JPG"
+  }
+]);
