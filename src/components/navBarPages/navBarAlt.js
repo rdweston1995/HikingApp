5847d663d0cddc025f5704b8abc3282505058db7
Robert <robert.d.weston@gmail.com>
2021-02-18 13:29:33 -0800

    Working on search bar for all non home pages navbar

---
+++ src/components/navBarPages/navBarAlt.js
@@ -0,0 +1,40 @@
+//Node imports
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import {withRouter} from "react-router-dom";
+import DropdownBtn from "react-bootstrap/DropdownButton"
+import Dropdown from "react-bootstrap/Dropdown"
+import loadImages from "../loadImages/loadImages";
+import Hamburger from "../hamburger/hamburger";
+
+//Component
+import Search from "./../../components/search/search";
+
+import "./navBarAlt.css";
+
+function navBarAlt(props) {
+    return(
+        <Row id="navBar" className="navBar">
+            <Col xs log="4">
+                <img className="logo" src={loadImages['bearable']} alt="Logo" onClick={() => props.history.push('/')}/>
+                <h1 className="navBarTitle" onClick={() => props.history.push('/')}>Bearable</h1>
+            </Col>
+            <Col>
+                <Search />
+            </Col>
+            <Col>
+                <DropdownBtn id="dropdown-basic-button" className="testBtn" title={<Hamburger/>} styles={{float: 'right'}}>
+                <Dropdown.Item className="navBarTag tagExplore" onClick={() => props.history.push('/explore')}>Explore</Dropdown.Item>
+                <Dropdown.Item className="navBarTag tagPlan" onClick={() => props.history.push('/plan')}>Plan</Dropdown.Item>
+                <Dropdown.Item className="navBarTag tagTips" onClick={() => props.history.push('/tips')}>Tips</Dropdown.Item>
+                <Dropdown.Item className="navBarTag tagHelp" href="https://github.com/rdweston1995/HikingApp" target="_blank" rel="noopener noreferrer">Help</Dropdown.Item>
+                </DropdownBtn>
+            </Col>
+
+        </Row>
+    );
+}
+
+export default withRouter(navBarAlt);
--- src/pages/plan/plan.js
+++ src/pages/plan/plan.js
@@ -2,16 +2,24 @@
 import React from "react";
 import {withRouter} from "react-router-dom";
 
-import NavBar from "./../../components/navBar/navBar";
+// import NavBar from "./../../components/navBar/navBar";
+import NavBarAlt from "./../../components/navBarPages/navBarAlt";
 
 //CSS
 import "./plan.css";
 
 class Plan extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        };
+    }
     render() {
         return (
             <div id="planPage">
-                <NavBar />
+                <NavBarAlt 
+                    history={this.state.history}/>
                 <div id="planDiv">
                     <p className="comingSoon">Coming Soon</p>
                 </div>
